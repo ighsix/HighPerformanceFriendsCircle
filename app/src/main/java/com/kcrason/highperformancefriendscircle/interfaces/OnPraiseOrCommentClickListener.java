2223eaf8ddfc346ae1e5c2f8145012073860bf1a
535089696@qq.com <Xzl@110892$123&>
2018-07-02 15:28:56 +0800

    更新apk文件

--- app/src/main/java/com/kcrason/highperformancefriendscircle/interfaces/OnPraiseOrCommentClickListener.java
+++ app/src/main/java/com/kcrason/highperformancefriendscircle/interfaces/OnPraiseOrCommentClickListener.java
@@ -3,5 +3,6 @@ package com.kcrason.highperformancefriendscircle.interfaces;
 public interface OnPraiseOrCommentClickListener {
     void onPraiseClick(int position);
 
+
     void onCommentClick(int position);
 }
